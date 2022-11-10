100452d9cf43599037e0a4493441c2346a0d7c6c
burcusevinc <sburcusevinc@gmail.com>
2022-11-10 20:13:16 +0300

    homework 1 - clock

--- Javascript Homeworks/task1-js-clock/js/clock.js
+++ Javascript Homeworks/task1-js-clock/js/clock.js
@@ -1,6 +1,5 @@
 // Girişte ismimizi sorsun:
 let enteredName = prompt("İsminizi Giriniz: ");
-
 // İsmi yazdıracağımız elementi bulalım
 let myName = document.querySelector("#myName");
 // İsmin baş harfini büyük yapalım
@@ -8,14 +7,30 @@ enteredName = enteredName[0].toUpperCase() + enteredName.substring(1);
 // myName elementine girilen stringi yazalım.
 myName.innerHTML = `${enteredName}`;
 
-function getDate() {
-  // Tarih ve saati alalım
-  let date = new Date().toLocaleString("tr-TR");
-  // Tarih ve saati yazdıracağımız elementi bulalım
-  let myClock = document.querySelector("#myClock");
-  // myClock elementine tarih-saat bilgisini yazdıralım
-  // Bu şekilde o anki sabit tarih ve saat yazdırılır
-  myClock.innerHTML = `${date}`;
+
+// günler arrayi
+let days=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+function getDate(){
+    let date = new Date();
+    //günler
+    //getDay fonksiyonu 0-6 arası değerler verir
+    //oluşturduğumuz days arrayine bu değerleri atadık, 
+    //örneğin bugün günlerden perşembe, 4 indeksine karşılık geliyor.
+    let d = days[date.getDay()];
+    //saat
+    let h = date.getHours();
+    //dakika
+    let m = date.getMinutes();
+    //saniye
+    let s = date.getSeconds();
+    // tarih bilgisini yazdıracağımız elementi seçelim
+    let time = document.querySelector("#myClock")
+    // eğer saat,dakika veya saniye 10'dan küçük ise başına 0 ekler -> 05,03 gibi. 16:05:03
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    //time elementine yazılacak bilgi verildi
+    time.innerHTML = `${h}:${m}:${s} ${d}`
 }
 
 // Bu fonksiyonu 1 saniye içinde yenilersek, tarih-saat anlık olarak güncellenir.
